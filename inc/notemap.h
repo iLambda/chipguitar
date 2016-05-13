71da1c79e114dc29daaab1563256e7d293dfc86f
Freeman <ilambda@hotmail.fr>
2016-05-13 03:07:51 +0200

    Added notemap.h, which contains note/key mappings

---
+++ inc/notemap.h
@@ -0,0 +1,7 @@
+#include "keycodes.h"
+#define NOTEMAP_LENGTH 16
+
+unsigned char notemap_default[NOTEMAP_LENGTH] = {
+         NC1_C, NC1_D, NC1_E, NC1_F, NC1_G, NC1_A, NC1_B, NC2_C,
+         NC1_Cs, NC1_Ds, NC1_E, NC1_Fs, NC1_Gs, NC1_As, NC1_B, NC2_Cs
+};
